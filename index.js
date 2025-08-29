c7cceb40051480b2775b54989e7f8afe536143e6
mahfuzh129 <mahfuzh266@gmail.com>
2025-08-29 12:26:08 +0600

    add heart-count functionality

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+// incresing heart-count
+const favCount1= document.getElementById('fav-count');
+let favCount =0;
+
+const favIcons= document.querySelectorAll('.fav-icon');
+favIcons.forEach(icon =>{
+  icon.addEventListener('click', function(e) {
+    e.preventDefault();
+    favCount++;
+    favCount1.innerText = favCount;
+  });
+});
+
